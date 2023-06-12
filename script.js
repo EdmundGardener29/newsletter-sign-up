03212a2386b5643779e80a646ad9795ec119be85
Edmund Gardener <edmund.gardener@amalitech.com>
2023-06-12 16:32:48 +0000

    Additional Changes

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const form = document.getElementById("form");
 const button = document.querySelector(".submit");
+const success = document.getElementById("success");
+const dismisbtn = document.getElementById("dismissbtn");
+const errorMessage = document.getElementById("error-message");
 
 
-
-const handleSubmit = () => { 
+const handleSubmit = () => {
   
 
   const emailInput = document.getElementById("email");
@@ -16,10 +18,45 @@ const handleSubmit = () => {
     return;
   } else {
 
-    alert("Please enter a valid email address.");
+     alert("This email is invalid.");
   }
+    
+  dismisbtn.addEventListener('click', function () {
+    success.remove();
+  })
+  console.log(success);
 }
+
+
 function validateEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 }
+
+
+// function handleSubmit(event) {
+
+//   const emailInput = document.getElementById("email");
+//   const email = emailInput.value;
+
+//   if (validateEmail(email)) {
+//     // Clear any existing error message
+//     document.getElementById("error-message").textContent = "";
+
+//     // Form submission logic here
+//     window.location.href = "success.html";
+//   } else {
+//     // Display the error message in the form space
+//     document.getElementById("error-message").textContent =
+//       "Invalid email format.";
+//   }
+// }
+
+
+
+// function validateEmail(email) {
+//   // Regular expression pattern to validate email format
+//   var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//   return pattern.test(email);
+// }

81f4402bd33e7d81574b45343910895138603193
Edmund Gardener <edmund.gardener@amalitech.com>
2023-06-21 09:53:44 +0000

    updated feature

--- script.js
+++ script.js
@@ -1,60 +1,51 @@
-const form = document.querySelector("form");
-const email = document.getElementById("email");
-const error = document.getElementById("error");
-form.addEventListener("submit", handleFormSubmit);
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const invalidEmailMsg = document.getElementById("invalid-email");
+const confirmedMessage = document.getElementById("confirmed-message");
+const userEmailAddress = document.getElementById("user-email");
+const dismissBtn = document.getElementById("dismiss-btn");
 
-email.addEventListener("invalid", (e) => {
-  e.preventDefault();
 
-  error.textContent = "Valid email required";
-  error.style.display = "block";
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  
+  if (!validateEmail(emailInput.value)) {
+    invalidEmailMsg.style.display = "block";
+    return;
+  }
+
+
+  invalidEmailMsg.style.display = "none";
+
+ 
+  confirmedMessage.style.display = "block";
+  document.getElementById("container").style.display = "none";
+  userEmailAddress.textContent = emailInput.value;
+
+  confirmedMessage.classList.remove("hide");
 });
 
-function handleFormSubmit(event) {
-  event.preventDefault();
 
-  const emailInput = document.querySelector("#email");
-  const email = emailInput.value;
+dismissBtn.addEventListener("click", function () {
+  confirmedMessage.style.display = "none";
+});
 
-  if (validateEmail(email)) {
-    const container = document.querySelector(".container");
-    container.style.display = "none";
-
-// document.getElementById("form").addEventListener("submit", (e) => {
-//   for (var i = 0; i < document.querySelectorAll("#form input").length; i++) {
-//     if (document.querySelectorAll("form input")[i].value === "") {
-//       e.preventDefault();
-
-//       document.querySelectorAll("form input")[i].style.backgroundColor = "pink";
-//       document.querySelectorAll("form input")[i].placeholder = "Required";
-//     } else {
-//       document.querySelectorAll("form input")[i].style.backgroundColor =
-//         "#fff";
-//     }
-//   }
-// });
-
-    const successContainer = document.querySelector(".container-2");
-    successContainer.classList.remove("hidden");
-
-     const emailParagraph = successContainer.querySelector(".confirm-msg");
-     emailParagraph.innerHTML = `A confirmation email has been sent to <span class='user-email'>${email}</span>. Please open it and click the button inside to confirm your subscription.`;
-
-    const dismissButton = successContainer.querySelector("button");
-    dismissButton.addEventListener("click", function () {
-      window.location.reload();
-    });
-  }
-}
 
 function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
 
-function handleDismissButtonClick() {
-  const successContainer = document.querySelector(".container-2");
-  successContainer.style.display = "none";
+
+// form.addEventListener("submit", handleSubmit);
+
+function dismissMessage() {
+  successMessage.classList.add("hide");
 }
 
+const dismissButton = confirmedMessage.querySelector('button');
+dismissButton.addEventListener('click', function () {
+ window.location.reload(); 
+})
